278c3aa6691d72bfd01b28a4fea2790db618c859
Danil Myshakin <Tyuiu.MyshakinD@outlook.com>
2024-10-23 03:26:06 +0500

    Task2

--- Tyuiu.MyshakinD.Sprint3.Task2.V1.Lib/DataService.cs
+++ Tyuiu.MyshakinD.Sprint3.Task2.V1.Lib/DataService.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.MyshakinD.Sprint3.Task2.V1.Lib
                 temp = Math.Sin(startValue) * Math.Pow(0.5, 2);
                 res += temp;
                 startValue++;
-            } while (startValue < stopValue);
+            } while (startValue < stopValue + 1);
             return Math.Round(res, 3);
         }
     }
--- Tyuiu.MyshakinD.Sprint3.Task2.V1.Test/DataServiceTest.cs
+++ Tyuiu.MyshakinD.Sprint3.Task2.V1.Test/DataServiceTest.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.MyshakinD.Sprint3.Task2.V1.Test
         public void TestMethod1()
         {
             DataService ds = new DataService();
-            double wait = Math.Round(0.48880237052684505, 3);
+            double wait = 0.353;
             int x = 1; int y = 10;
             double res = ds.GetSumSeries(x, y);
             Assert.AreEqual(wait, res);

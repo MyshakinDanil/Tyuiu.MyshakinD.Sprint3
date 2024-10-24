e3fda2da06ea7ed96dfe041feccfed66d0ffbfea
Danil Myshakin <Tyuiu.MyshakinD@outlook.com>
2024-10-24 19:22:41 +0500

    Task7

---
+++ Tyuiu.MyshakinD.Sprint3.Task7.V10.Lib/DataService.cs
@@ -0,0 +1,33 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.MyshakinD.Sprint3.Task7.V10.Lib
+{
+    public class DataService : ISprint3Task7V10
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            double temp = 0;
+            int len = stopValue - startValue + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                temp = Math.Sin(x) + 3;
+                if (temp == 0)
+                {
+                    valueArray[count] = 0;
+                    count++;
+                }
+                else
+                {
+                    y = ((5 * x + 2.5) / temp) + 2 * x + Math.Cos(x);
+                    valueArray[count] = Math.Round(y, 2);
+                    count++;
+                }
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.MyshakinD.Sprint3.Task7.V10/Program.cs
@@ -0,0 +1,54 @@
+﻿using Tyuiu.MyshakinD.Sprint3.Task7.V10.Lib;
+
+namespace Tyuiu.MyshakinD.Sprint3.Task7.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            
+            Console.Title = "Спринт #3 | Выполнил: Мышакин Д. | ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Мышакин Данил              | ПКТб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу значений функции: (функция  *");
+            Console.WriteLine("* (произвести табулирование) f(x) в заданном диапазоне [-5; 5] с шагом 1. *");
+            Console.WriteLine("* Произвести проверку деления на ноль. При делении на ноль вернуть        *");
+            Console.WriteLine("* значение 0. Значения занести в массив. Значения округлить до 2 знаков.  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|    X     |   f(x)   |");
+            Console.WriteLine("+----------+----------+");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}     |  {1,6:f2}  |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+----------+");
+        }
+    }
+}
